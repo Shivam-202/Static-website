10dbd1f1740e58ed38e9a09ebfcb87d802c9d010
Shivam-202 <83532229+Shivam-202@users.noreply.github.com>
2021-12-10 14:53:29 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,53 @@
+
+function backchange1(){
+    document.getElementById('prosec-1').style.background="pink";
+    document.getElementById('probtn-1').style.background= "rgb(233, 233, 229)";
+    document.getElementById('probtn-1').style.color= "black";
+}
+function backchange2(){
+    document.getElementById('prosec-2').style.background="rgb(110, 20, 158)";
+    document.getElementById('pt-2').style.color="white";
+    document.getElementById('pp-2').style.color="white";
+    document.getElementById('probtn-2').style.background= "rgb(233, 233, 53)";
+    document.getElementById('probtn-2').style.color= "black";
+}
+function backchange3(){
+    document.getElementById('prosec-3').style.background="red";
+    document.getElementById('pt-3').style.color="white";
+    document.getElementById('pp-3').style.color="white";
+    document.getElementById('probtn-3').style.background= "rgb(233, 233, 212)";
+    document.getElementById('probtn-3').style.color= "black";
+}
+function backchange4(){
+    document.getElementById('prosec-4').style.background="green";
+    document.getElementById('pt-4').style.color="white";
+    document.getElementById('pp-4').style.color="white";
+    document.getElementById('probtn-4').style.color= "black";
+}
+
+function blacken(){
+    document.getElementById('bodies').style.background="black";
+    document.getElementById('bodies').style.color="white";
+    document.getElementById('black').style.backgroundColor="black";
+    document.getElementById('black').style.color="white";
+    document.getElementById('black').style.borderColor="white";
+    document.getElementById('black').style.display="none";
+    document.getElementById('white').style.display="unset";
+}
+
+function whiten(){
+    document.getElementById('bodies').style.background="white";
+    document.getElementById('bodies').style.color="black";
+    document.getElementById('white').style.backgroundColor="black";
+    document.getElementById('white').style.color="white";
+    document.getElementById('white').style.borderColor="black";
+    document.getElementById('white').style.display="none";
+    document.getElementById('black').style.display="unset";
+
+    document.getElementById('black').style.backgroundColor="white";
+    document.getElementById('black').style.color="black";
+    document.getElementById('black').style.borderColor="black";
+    
+}
+
+//  ********* ///////// ********* $$$$$$$$ ********** ######## ********  &&&&&&&  
